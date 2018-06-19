67848abbfdb0aef2ae7aa589e02a10b7ca84d775
Matt <mg11g16@soton.ac.uk>
2018-06-19 03:12:10 +0100

    Implent controls for playback
    Prev, Next, Play/Pause, Seeking

---
+++ src/components/PlaybackControls.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+import FontAwesome from 'react-fontawesome';
+import './css/PlaybackControls.css';
+
+class PlaybackControls extends Component {
+  constructor(props) {
+    super(props);
+
+    this.musicKit = window.MusicKit.getInstance();
+
+    this.state = {
+      playbackState: window.MusicKit.PlaybackStates.none
+    }
+
+    this.playbackStateDidChange = this.playbackStateDidChange.bind(this);
+    this.musicKit.addEventListener(window.MusicKit.Events.playbackStateDidChange, this.playbackStateDidChange);
+  }
+
+  playbackStateDidChange = (event) => {
+    this.setState({playbackState: event.state});
+  }
+
+  play = () => {
+    this.musicKit.player.play();
+  }
+
+  pause = () => {
+    this.musicKit.player.pause();
+  }
+
+  previous = () => {
+    this.musicKit.player.skipToPreviousItem();
+  }
+
+  next = () => {
+    this.musicKit.player.skipToNextItem();
+  }
+
+  formatTime(value) {
+    var minutes = Math.floor(value / 60);
+    var seconds = value - (minutes * 60);
+
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+
+    return minutes + ":" + seconds;
+  }
+
+  getPlayPause = () => {
+    if (this.state.playbackState !== window.MusicKit.PlaybackStates.playing) {
+      return (
+          <Button className="control" onClick={this.play}><FontAwesome name="play" size="2x"/></Button>
+      )
+    } else {
+      return (
+          <Button className="control" onClick={this.pause}><FontAwesome name="pause"  size="2x"/></Button>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <div className="controls">
+        <span className="time">{this.formatTime(this.props.currentPlaybackTime)}</span>
+        <Button className="control" onClick={this.previous}><FontAwesome name="backward" size="2x" /></Button>
+        {this.getPlayPause()}
+        <Button className="control" onClick={this.next}><FontAwesome name="forward" size="2x"/></Button>
+        <span className="time-right">{this.formatTime(this.props.currentPlaybackDuration)}</span>
+      </div>
+    )
+  }
+}
+
+export default PlaybackControls;
